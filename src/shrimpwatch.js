6866ab543c967fd27ba7009d5b08a411d03ae5be
jjreedv <87283488+JohnReedV@users.noreply.github.com>
2023-01-27 14:40:53 -0800

    add btc workers

--- src/shrimpwatch.js
+++ src/shrimpwatch.js
@@ -1,42 +1,101 @@
 import { Worker } from 'worker_threads'
 import * as fs from 'fs'
 import Web3 from 'web3'
+import { btcQueries } from './btcQueries.js'
 
 class ShrimpWatch {
+
     async start() {
         const conf = JSON.parse(fs.readFileSync('./conf.json', 'utf8'))
-        const web3 = new Web3(new Web3.providers.HttpProvider(conf.ethHttpProvider))
-        const numWorkers = conf.workerPoolSize
-        let workers = []
-        let completedBlocks = new Set()
+        let numWorkers = Math.round(conf.workerPoolSize)
 
-        for (let i = 0; i < numWorkers; i++) {
-            let blockNumber = conf.startblock + i
-            while (completedBlocks.has(blockNumber)) {
-                blockNumber++
-            }
-            completedBlocks.add(blockNumber)
-            const worker = new Worker('./src/eth.js')
-            worker.postMessage({ blockNumber })
-            workers.push(worker)
+        if (conf.btcOn && conf.ethOn) {
+            if (numWorkers % 2 !== 0) {
+                numWorkers = (numWorkers - 1) / 2
+                console.log(`Number of workers is not divisible by 2, using ${numWorkers} workers for each blockchain`)
+            } else { numWorkers = numWorkers / 2 }
+
+            this.computeBtc(numWorkers, conf)
+            this.computeEth(numWorkers, conf)
+        } else if (conf.btcOn) {
+            this.computeBtc(numWorkers, conf)
+        } else if (conf.ethOn) {
+            this.computeEth(numWorkers, conf)
         }
 
-        // Wait for all workers to complete
-        for (let worker of workers) {
+    }
+
+    async computeBtc(numWorkers, conf) {
+        const btcQ = new btcQueries()
+        let btcWorkers = []
+        let btcCompletedBlocks = new Set()
+        
+        // assign work
+        for (let i = 0; i < numWorkers; i++) {
+            let blockNumber = conf.btcstartblock + i
+            while (btcCompletedBlocks.has(blockNumber)) {
+                blockNumber++
+            }
+            btcCompletedBlocks.add(blockNumber)
+            const btcWorker = new Worker('./src/btc.js')
+            btcWorker.postMessage({ blockNumber })
+            btcWorkers.push(btcWorker)
+        }
+
+        for (let worker of btcWorkers) {
             worker.on('message', async (message) => {
                 if (message.done) {
+                    // If at top of chain, wait for next block
+                    let latestBlockNumber = await btcQ.getBlockHeight()
+                    while (message.blockNumber >= latestBlockNumber) {
+                        latestBlockNumber = await btcQ.getBlockHeight()
+                    }
+                    // reassign work
+                    let blockNumber = message.blockNumber++
+                    while (btcCompletedBlocks.has(blockNumber)) {
+                        blockNumber++
+                    }
+                    btcCompletedBlocks.add(blockNumber)
+                    worker.postMessage({ blockNumber })
+                }
+            })
+        }
+
+    }
+
+    async computeEth(numWorkers, conf) {
+        const web3 = new Web3(new Web3.providers.HttpProvider(conf.ethHttpProvider))
+        let ethWorkers = []
+        let ethCompletedBlocks = new Set()
+
+        // assign work
+        for (let i = 0; i < numWorkers; i++) {
+            let blockNumber = conf.ethstartblock + i
+            while (ethCompletedBlocks.has(blockNumber)) {
+                blockNumber++
+            }
+            ethCompletedBlocks.add(blockNumber)
+            const ethWorker = new Worker('./src/eth.js')
+            ethWorker.postMessage({ blockNumber })
+            ethWorkers.push(ethWorker)
+        }
+
+        for (let worker of ethWorkers) {
+            worker.on('message', async (message) => {
+                if (message.done) {
+                    // If at top of chain, wait for next block
                     let latestBlock = await web3.eth.getBlock('latest')
                     let latestBlockNumber = latestBlock.number
-                    // If at top of chain, wait for next block and assign work
                     while (message.blockNumber >= latestBlockNumber) {
                         latestBlock = await web3.eth.getBlock('latest')
                         latestBlockNumber = latestBlock.number
                     }
+                    // reassign work
                     let blockNumber = message.blockNumber++
-                    while (completedBlocks.has(blockNumber)) {
+                    while (ethCompletedBlocks.has(blockNumber)) {
                         blockNumber++
                     }
-                    completedBlocks.add(blockNumber)
+                    ethCompletedBlocks.add(blockNumber)
                     worker.postMessage({ blockNumber })
                 }
             })
