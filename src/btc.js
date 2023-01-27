66e32396d1d8ce978fd0c9956c9103f3afb4f6ff
jjreedv <87283488+JohnReedV@users.noreply.github.com>
2023-01-27 14:41:42 -0800

    start btc scanning

--- src/btc.js
+++ src/btc.js
@@ -1,18 +1,37 @@
+import { parentPort } from 'worker_threads'
 import { btcQueries } from './btcQueries.js'
 import { DBHandler } from './dbHandler.js'
+import * as fs from 'fs'
 
 class Btc {
+    conf
     db
     btcQ
+    blockNumber
 
     constructor() {
         this.btcQ = new btcQueries()
         this.db = new DBHandler()
+        this.conf = JSON.parse(fs.readFileSync('./conf.json', 'utf8'))
+
+        parentPort.on('message', (message) => {
+            this.blockNumber = message.blockNumber
+            console.log("Working on BTC block : " + this.blockNumber)
+            this.startBtc()
+        })
     }
 
     async startBtc() {
-        console.log('btc on')
+        let blockHash = await this.btcQ.getBlockHash(this.blockNumber)
+        let block = await this.btcQ.getBlock(blockHash)
+
+        for (let i = 0; i < block.tx.length; i++) {
+            let rawTX = await this.btcQ.getRawTX(block.tx[i], blockHash)
+            //console.log(rawTX)
+        }
+
+        parentPort.postMessage({ done: true, blockNumber:  this.blockNumber})
     }
 }
 
-new Btc().startBtc()
+new Btc()
