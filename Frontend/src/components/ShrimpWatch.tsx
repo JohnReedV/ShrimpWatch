f3de7caf8d077ac3a64d3ee333a3416eb08a0e78
John Reed <87283488+JohnReedV@users.noreply.github.com>
2023-02-08 12:02:24 -0800

    remove react clutter

---
+++ Frontend/src/components/ShrimpWatch.tsx
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+import '../App.css'
+
+function App() {
+  
+    return (
+      <div className="App">
+        <div>
+          <a href="https://shrimpwatch.com" target="_blank">
+            <img src="src/assets/shrimp256.svg" className="logo" alt="ShrimpWatch Logo" />
+          </a>
+        </div>
+        <h1>ShrimpWatch</h1>
+      </div>
+    )
+  }
+  
+  export default App
