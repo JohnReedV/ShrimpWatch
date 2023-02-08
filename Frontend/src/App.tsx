ff1983c6bd15a663a1a3134a1cb59f2108e9d250
John Reed <87283488+JohnReedV@users.noreply.github.com>
2023-02-07 16:20:01 -0800

    init frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://shrimpwatch.com" target="_blank">
+          <img src="src/assets/shrimp256.svg" className="logo" alt="ShrimpWatch Logo" />
+        </a>
+      </div>
+      <h1>ShrimpWatch</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
