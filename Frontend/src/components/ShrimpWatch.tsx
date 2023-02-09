7043ff8d29153a491ddf0514b330043bd3a00b9a
John Reed <87283488+JohnReedV@users.noreply.github.com>
2023-02-08 16:33:21 -0800

    root component

--- Frontend/src/components/ShrimpWatch.tsx
+++ Frontend/src/components/ShrimpWatch.tsx
@@ -6,12 +6,13 @@ class ShrimpWatch extends Component {
   render() {
     return (
       <div className="ShrimpWatch">
-        <div>
-          <a href="https://shrimpwatch.com" target="_blank">
-            <img src="src/assets/shrimp512.svg" className="logo" alt="ShrimpWatch Logo" />
+        <h1>ShrimpWatch
+        <a href="https://shrimpwatch.com" target="_blank">
+            <img src="src/assets/shrimp512.png" className="logo" alt="ShrimpWatch Logo" />
           </a>
-        </div>
-        <h1>ShrimpWatch</h1>
+        </h1>
+        <button className="btcbutton"> </button>
+        <button className="ethbutton"> </button>
       </div>
     )
   }
