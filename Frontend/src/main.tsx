16e8a39d54ee44a9f897bab05a4fd24cd4d2d63e
John Reed <87283488+JohnReedV@users.noreply.github.com>
2023-02-24 12:05:20 -0800

    fix duplicate render/query

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -4,7 +4,5 @@ import ShrimpWatch from './components/ShrimpWatch'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <ShrimpWatch />
-  </React.StrictMode>,
 )
